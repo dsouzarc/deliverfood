2658efa37e1d625fc54e845619d4bc099cb885ac
Ryan D'souza <dsouzarc@gmail.com>
2014-09-13 20:24:29 -0400

    TextViews/LinearLayouts initialized

--- app/src/main/java/com/ryan/deliverfood/ViewOrderActivity.java
+++ app/src/main/java/com/ryan/deliverfood/ViewOrderActivity.java
@@ -1,17 +1,40 @@
 package com.ryan.deliverfood;
-
+import android.content.Context;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.ryan.deliverfood.R;
-
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.LinearLayout;
 public class ViewOrderActivity extends Activity {
+
+    private Order theOrder;
+    private Context theC;
+    private LinearLayout orderItemsLayout;
+    private Button orderStatus;
+    private TextView clientName, clientAddress, clientPhone, restaurantName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_order);
+        initializeVariables();
+    }
+
+    private void initializeVariables() {
+        this.theOrder = Order.getOrder(getIntent().getExtras().getString("Order"));
+
+        this.orderItemsLayout = (LinearLayout) findViewById(R.id.orderItemsLayout);
+        this.orderStatus = (Button) findViewById(R.id.orderStatus);
+        this.clientName = (TextView) findViewById(R.id.clientNameTV);
+        this.clientAddress = (TextView) findViewById(R.id.clientAddress);
+        this.clientPhone = (TextView) findViewById(R.id.clientPhone);
+        this.restaurantName = (TextView) findViewById(R.id.restaurantTV);
+    }
+
+    private TextView getOrderItem(final String text) {
     }
 
 
